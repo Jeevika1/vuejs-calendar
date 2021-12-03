00752ccc8045fa99cda6110f5e495c68415b1b99
Jeevika Pal <jeevika@cxunicorn.com>
2021-12-03 12:09:08 +0530

    Load stored events

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 import moment from 'moment-timezone';
 moment.tz.setDefault('UTC')
+import Axios from 'axios'
 
 export default new Vuex.Store({
     state: {
@@ -34,13 +35,28 @@ export default new Vuex.Store({
             state.eventFormActive = payload
         },
         addEvent(state, payload) {
-            state.events.push({
-                description: payload,
-                date: state.eventFormDate
-            })
+            state.events.push(payload)
         },
         eventFormDate(state, payload) {
             state.eventFormDate = payload
+        }
+    },
+    actions: {
+        addEvent(context, payload) {
+            return new Promise((resolve, reject) => {
+                let obj = {
+                    description: payload,
+                    date: context.state.eventFormDate
+                }
+                Axios.post('/add_event', obj).then(response => {
+                    if(response.status === 200) {
+                        context.commit('addEvent', obj)
+                        resolve()
+                    } else {
+                        reject()
+                    }
+                })
+            }) 
         }
     }
   })
